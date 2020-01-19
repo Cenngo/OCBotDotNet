4a5eff18240f31e942830c5ff1475da683b32131
uras <uo7@njit.edu>
2020-01-18 21:41:23 -0500

    Implemented user data, language data, and commands for registering a language, showing user data and insulting user.

---
+++ DiscordNET/Data/UserData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace DiscordNET.Data
+{
+    public struct UserCollection
+    {
+        [JsonProperty("Users")]
+        public List<userData> userList { get; set; }
+    }
+    public class userData
+    {
+        //FIXME: non-private setters
+        [JsonProperty("ID")]
+        public ulong discordID { get; set; }
+
+        [JsonProperty("handle")]
+        public string dHandle { get; set; }
+
+        [JsonProperty("lang")]
+        public string langauge { get; set; }
+
+        public userData()
+        {
+            discordID = ulong.MinValue;
+            dHandle = string.Empty;
+            langauge = "default";
+        }
+    }
+
+    class Insult
+    {
+        public List<string> SupportedLanguages = new List<string>
+        {
+            "tr","en"
+        };
+
+        [JsonProperty("TR")]
+        public List<String> TR_insults { get; private set; }
+        [JsonProperty("EN")]
+        public List<String> EN_insults { get; private set; }
+    }
+}
